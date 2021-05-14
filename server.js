a1802f533da8a8ccd63f4c9765d0d01599b9057a
Amal Khan <amalfkhan@gmail.com>
2021-05-13 21:33:54 -0700

    added shorturl post

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
 const urlScheme = new Scheme({
@@ -27,28 +27,32 @@ const urlScheme = new Scheme({
 const Url = mongoose.model('Url', urlScheme);
 
 app.post('/api/shorturl', (req, res) => {  
-  if(!req.body.url.includes('http')) {
-    res.json({ error: 'invalid url' });
-
-  } else {
-    Url.estimatedDocumentCount((docCountError, count) => {
-        if (docCountError){
-            console.error(docCountError)
-        }else{
-          const url = new Url({ original_url: req.body.url, short_url: count });
-          url.save((saveError, data) => {
-            if(saveError) { 
-              return console.error(saveError)
-            } else {
-              res.json({ 
-                original_url: data.original_url,
-                short_url: data.short_url
-              });
-            }
+  //add handling of invalid urls
+  Url.estimatedDocumentCount((docCountError, count) => {
+    if (docCountError){
+        console.error(docCountError)
+    }else{
+      const url = new Url({ original_url: req.body.url, short_url: count });
+      url.save((saveError, data) => {
+        if(saveError) { 
+          return console.error(saveError)
+        } else {
+          res.json({ 
+            original_url: data.original_url,
+            short_url: data.short_url
           });
         }
-    });
-  }
+      });
+    }
+  });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const searchUrl = req.params.short_url;
+  Url.findOne({ short_url: searchUrl }, (searchError, data) => {
+    if (searchError) return console.error(err);
+    res.redirect(data.original_url)
+  });
 });
 
 app.listen(port, () => {
